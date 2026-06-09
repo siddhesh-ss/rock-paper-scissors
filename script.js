3221fc619cb3812c05918dee8905f0f62f8e3910
siddhesh-ss <siddheshshelar32@gmail.com>
2026-06-09 16:17:53 +0000

    Added whole UI

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 let humanScore = 0;
 let computerScore = 0;
+let gameCount = 0;
 
-const userName = prompt("Enter your user-Name : ");
+// for UI changes
+let dialogue = document.getElementById("dialogue");
+let youScore = document.getElementById("youScore");
+let compScore = document.getElementById("compScore");
+let youChoice = document.getElementById("youChoice");
+let compChoice = document.getElementById("compChoice");
 
-function getHumanChoice() {
-    let playerChoice = prompt("Enter your choice : ");
-    playerChoice = playerChoice.toLowerCase();
-    return playerChoice;
-}
+const btn = document.querySelectorAll(".btn");
+let playAgain = document.getElementById("playAgain");
+playAgain.style.display = "none";
+
+dialogue.classList.add("dialogue");
+// const userName = prompt("Enter your user-Name : ");
+
+// function getHumanChoice() {
+//     let playerChoice = prompt("Enter your choice : ");
+//     playerChoice = playerChoice.toLowerCase();
+//     return playerChoice;
+// }
 
 function getComputerChoice() {
     let rand = 100*Math.random();
@@ -17,21 +30,45 @@ function getComputerChoice() {
 }
 
 function playRound(humanChoice, computerChoice) {
-    humanChoice = getHumanChoice();
-    computerChoice = getComputerChoice();
+    // humanChoice = getHumanChoice();
+    // computerChoice = getComputerChoice();
+    gameCount++;
+
     if(humanChoice == computerChoice) {
-        humanScore++;
-        computerScore++;
-        return "It's Tie!";
+        dialogue.textContent = "It's a Tie!";
     }
     else if((humanChoice == "rock" && computerChoice == "scissor") || (humanChoice == "scissor" && computerChoice == "paper") || (humanChoice == "paper" && computerChoice == "rock")) {
         humanScore++;
-        return `You Won! ${humanChoice} beats ${computerChoice}`;
+        dialogue.textContent = `You Won! ${humanChoice} beats ${computerChoice}`;
     }
     else {
         computerScore++;
-        return `You Lose! ${computerChoice} beats ${humanChoice}`;
+        dialogue.textContent = `You Lose! ${computerChoice} beats ${humanChoice}`;
     }
+    updateUI(humanChoice, computerChoice);
+    if(gameCount == 6) {
+        endGame();
+    }
+
+}
+
+function updateUI(humanChoice, computerChoice) {
+    youChoice.innerHTML = `<img height="100px" src="images/${humanChoice}.png" alt="${humanChoice}">`;
+    compChoice.innerHTML = `<img height="100px" src="images/${computerChoice}Comp.png" alt="${computerChoice}">`
+    //update score
+    youScore.textContent = `score : ${humanScore}`;
+    compScore.textContent = `score : ${computerScore}`;
+}
+
+function endGame() {
+    if(humanScore > computerScore) dialogue.textContent = "you are the WINNER!";
+    else if(computerScore > humanScore) dialogue.textContent = "computer is the WINNER!";
+    else dialogue.textContent = "Wohh! It's Tie";
+    btn.forEach(button => {
+        button.disabled = true;
+    });
+
+    playAgain.style.display = "block";
 }
 
 function playGame() {
@@ -39,10 +76,42 @@ function playGame() {
         let result = playRound();
         console.log(result,"\n","Your score : ", humanScore, "Computer score : ", computerScore);
     }
-    if(humanScore > computerScore) return `${userName} is the WINNER!`;
-    else if(computerScore > humanScore) return "Computer is the WINNER!";
-    else return "Wohh! It's Tie";
 }
 
-const winner = playGame();
-console.log(winner.toUpperCase(),"\n", `${userName}'s score : `, humanScore, "Computer's score : ", computerScore);
+function reset() {
+    humanScore = 0;
+    computerScore = 0;
+    gameCount = 0;
+    youScore.textContent = "score : 0";
+    compScore.textContent = "score : 0";
+    dialogue.textContent = "";
+    youChoice.innerHTML = "";
+    compChoice.innerHTML = "";
+    btn.forEach(button => {
+        button.disabled = false;
+    });
+    playAgain.style.display = "none";
+}
+
+btn.forEach(button => {
+    button.addEventListener("click", () => {
+        playRound(button.id, getComputerChoice());
+    });
+});
+
+playAgain.addEventListener("click", reset);
+        // const playAgainDiv = document.createElement("div");
+        // const playAgainBtn = document.createElement("button");
+        // playAgainBtn.textContent = "PLAY AGAIN";
+        // playAgainBtn.style.fontSize = "18px";
+        // playAgainBtn.style.textAlign = "center";
+        // playAgainDiv.style.display = "flex";
+        // playAgainDiv.style.justifyContent = "center";
+        // playAgainDiv.appendChild(playAgainBtn);
+        // document.body.appendChild(playAgainDiv);
+    
+        // playAgainBtn.addEventListener("click", () => {
+        //     gameCount = 0;
+        // });
+// const winner = playGame();
+// console.log(winner.toUpperCase(),"\n", `${userName}'s score : `, humanScore, "Computer's score : ", computerScore);
